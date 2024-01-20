7436cf56b38232a4fbf9aa02c8be234e5aaa21fb
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2024-01-20 17:53:23 -0300

    move TitleInMemoryGateway to its own file

---
+++ src/enterprise/infra/gateways/title/title-in-memory.gateway.ts
@@ -0,0 +1,14 @@
+import { Title } from "../../../entities/title/title";
+import { TitleGateway } from "../../../application/gateways/title/title.gateway";
+
+export class TitleInMemoryGateway implements TitleGateway {
+  private static titles: Title[] = [];
+
+  async create(title: Title): Promise<void> {
+    TitleInMemoryGateway.titles.push(
+      new Title(title.isbn, title.author, title.subject, title.copiesInStock)
+    );
+
+    return Promise.resolve();
+  }
+}
--- tests/application/usecases/title/create-title.usecase.spec.ts
+++ tests/application/usecases/title/create-title.usecase.spec.ts
@@ -2,29 +2,17 @@ import { TitleGateway } from "../../../../src/enterprise/application/gateways/ti
 import { CreateTitleUseCase } from "../../../../src/enterprise/application/usecases/title/create-title.usecase";
 import { Title } from "../../../../src/enterprise/entities/title/title";
 import { ServerError } from "../../../../src/enterprise/shared/errors/server-error";
+import { TitleInMemoryGateway } from "../../../../src/enterprise/infra/gateways/title/title-in-memory.gateway";
 
 const baseTitle = new Title(1234567891011, 'Valid author', 'Valid subject', 0);
 
-const makeTitleGateway = () => {
-  class TitleInMemoryGateway implements TitleGateway {
-    private static titles: Title[] = [];
-
-    async create(title: Title): Promise<void> {
-      TitleInMemoryGateway.titles.push(
-        new Title(title.isbn, title.author, title.subject, title.copiesInStock)
-      );
-
-      return Promise.resolve();
-    }
-
-  }
-
+const makeTitleGatewaySpy = () => {
   const titleInMemoryGateway = new TitleInMemoryGateway();
 
   return titleInMemoryGateway;
 }
 
-const makeTitleGatewayWithError = () => {
+const makeTitleGatewaySpyWithError = () => {
   class TitleInMemoryGateway implements TitleGateway {
     async create(title: Title): Promise<void> {
       throw new Error();    
@@ -38,14 +26,14 @@ const makeTitleGatewayWithError = () => {
 
 describe("CreatetitleUseCase", () => {
   it("Should throw if dependency throws", async () => {
-    const sut = new CreateTitleUseCase(makeTitleGatewayWithError());
+    const sut = new CreateTitleUseCase(makeTitleGatewaySpyWithError());
     const promise = sut.execute(baseTitle);
 
     await expect(promise).rejects.toThrow(new ServerError());
   });
 
   it("should create the title", async () => {
-    const sut = new CreateTitleUseCase(makeTitleGateway());
+    const sut = new CreateTitleUseCase(makeTitleGatewaySpy());
     const expectedTitle = baseTitle;
     const actualTitle = await sut.execute(baseTitle);
 
