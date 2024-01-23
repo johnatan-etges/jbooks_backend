a643e017bcfca297f1acc36018f0454819187f09
Johnatan Matheus Etges <johnatan.etges@gmail.com>
2024-01-22 21:39:20 -0300

    remove incorrect directory

--- src/application/gateways/title/title.gateway.ts
+++
@@ -1,7 +0,0 @@
-import { Title } from "../../../enterprise/entities/title/title";
-
-export interface TitleGateway {
-  create(title: Title): Promise<void>;
-  findAll(): Promise<Title[]>;
-  findBySubject(subjectToSearch: string): Promise<Title[]>;
-}
